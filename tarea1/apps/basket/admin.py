175b44b3864d75b71ac48c6d0d70d150d8ae7ec9
Rygo <rygo.bustos@gmail.com>
2018-04-19 20:17:33 -0300

    Version final con modelos relacionados y administrador configurado

--- tarea1/apps/basket/admin.py
+++ tarea1/apps/basket/admin.py
@@ -6,16 +6,20 @@ from apps.basket.models import Team, Player, Coach, Match
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
     list_display=('name', 'description', 'logo', 'code')
+    search_fields = ('name',)
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
-    list_display=('name','nickname','birth_date','age_old','rut','email','height','weight','photo','positionGame')
+    list_display=('name','nickname','birthh_date','age_old','rut','email','height','weight','photo','positionGame')
+    search_fields = ('name', 'nickname','rut')
+    list_filter=('team','birthh_date')
 
 @admin.register(Coach)
 class CoachAdmin(admin.ModelAdmin):
     list_display=('name','age_old','email','rut','nickname')
+    list_filter=('team',)
 
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
-    pass
-    
+    list_display=('name','equipos')
+    list_filter=('teams',)
--- tarea1/apps/basket/models.py
+++ tarea1/apps/basket/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Team(models.Model):
     name= models.CharField(max_length=200)
     description= models.CharField(max_length=200)
-    logo=models.ImageField(upload_to='photos')
     code= models.CharField(max_length=200)
+    logo=models.ImageField(upload_to='apps/basket/photos/logo')
 
     def __str__(self):
         return self.name
@@ -13,13 +13,13 @@ class Team(models.Model):
 class Player(models.Model):
     name= models.CharField(max_length=50)
     nickname= models.CharField(max_length=50)
-    birth_date= models.DateField()
+    birthh_date= models.DateField()
     age_old=models.IntegerField()
     rut=models.CharField(max_length=30)
     email=models.CharField(max_length=50)
     height=models.IntegerField()
     weight=models.IntegerField()
-    photo=models.ImageField(upload_to='photos')
+    photo=models.ImageField(upload_to='apps/basket/photos/player')
 
     BASE='BA'
     ESCOLTA='EC'
@@ -39,6 +39,7 @@ class Player(models.Model):
         default=BASE,
     )
 
+    team=models.ForeignKey(Team, on_delete=models.CASCADE, null=True)
 
 
     def __str__(self):
@@ -51,11 +52,18 @@ class Coach(models.Model):
     rut=models.CharField(max_length=30)
     nickname= models.CharField(max_length=50)
 
+    team=models.ForeignKey(Team, on_delete=models.CASCADE, null=True)
+
     def __str__(self):
         return self.name
 
 class Match(models.Model):
     name= models.CharField(max_length=50)
+
+    teams= models.ManyToManyField(Team)
+
+    def equipos(self):
+        return " (V/S) ".join([str(t) for t in self.teams.all()])
 
     def __str__(self):
         return self.name
